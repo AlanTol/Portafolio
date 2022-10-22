37e06a17a661ada47f928c6a941bae22e36d010f
Alan <tolabaalan44@gmail.com>
2022-10-22 10:20:00 -0300

    primer commit

---
+++ js/copiar_portapapeles.js
@@ -0,0 +1,14 @@
+var botonCopiar = document.getElementById("copiar");
+var textoCopiar = document.getElementById("texto_encriptado");
+
+
+
+function copiarAlPortapapeles(){
+    let input = document.createElement("input");
+    input.setAttribute("value",textoCopiar.value);
+    document.body.appendChild(input);
+    input.select();
+    document.execCommand("copy");
+    document.body.removeChild(input)
+}
+botonCopiar.onclick = copiarAlPortapapeles;
